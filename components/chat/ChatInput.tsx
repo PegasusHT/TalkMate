7f8e901ba6b21ae7f06c107421e62117fc5eeaf3
jimmybui <jimmybui1995@gmail.com>
2024-10-01 17:00:24 +0700

    text input responsive

--- components/chat/ChatInput.tsx
+++ components/chat/ChatInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { View, TextInput, TouchableOpacity, ActivityIndicator, LayoutChangeEvent } from 'react-native';
 import { Send, Mic, Trash2 } from 'lucide-react-native';
 import RecordingAnimation from '@/components/chat/animation/recordingAnimation';
 import { primaryColor } from '@/constant/color';
@@ -27,6 +27,26 @@ const ChatInput: React.FC<ChatInputProps> = ({
   isProcessingAudio,
   stopAllAudio, 
 }) => {
+  const [isMultiline, setIsMultiline] = useState(false);
+  const inputRef = useRef<TextInput>(null);
+  const [inputWidth, setInputWidth] = useState(0);
+  const [inputHeight, setInputHeight] = useState(0);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.measure((x, y, width, height, pageX, pageY) => {
+        setInputWidth(width);
+        setInputHeight(height);
+      });
+    }
+  }, []);
+
+  const handleContentSizeChange = (event: {nativeEvent: {contentSize: {height: number}}}) => {
+    const { height } = event.nativeEvent.contentSize;
+    const newIsMultiline = height > inputHeight/4;
+    setIsMultiline(newIsMultiline);
+  };
+
   if (isProcessingAudio) {
     return (
       <View className="flex-row items-center justify-center p-2 border-t border-gray-200 h-20 mx-2">
@@ -57,14 +77,22 @@ const ChatInput: React.FC<ChatInputProps> = ({
   };
 
   return (
-    <View className="flex-row items-center p-2 border-t border-gray-200 h-20 mx-2">
+    <View 
+      className={`flex-row mb-[-4px] items-center p-2 border-t border-gray-200 mx-2 ${isMultiline ? 'min-h-[100px]' : 'h-20'}`}
+      onLayout={(event: LayoutChangeEvent) => {
+        setInputWidth(event.nativeEvent.layout.width - 80);
+        setInputHeight(event.nativeEvent.layout.height);
+      }}
+    >
       <TextInput
-        className="flex-1 bg-gray-100 rounded-full px-4 py-2 mr-2 h-full"
+        ref={inputRef}
+        className={`flex-1 bg-gray-100 px-4 py-2 mr-2 h-full ${isMultiline ? 'rounded-xl py-2' : 'rounded-full py-5'}`}
+        style={{ minHeight: 60, maxHeight: 160 }}
         value={message}
         onChangeText={setMessage}
-        multiline
-        numberOfLines={4}
+        multiline={true}
         placeholder="Type your message..."
+        onContentSizeChange={handleContentSizeChange}
       />
       <TouchableOpacity onPress={message ? handleSend : handleMicPressWithAudioStop}>
         {message ? (
