e085083d84652a4773525be69dccb1f110ec8e31
jimmybui <jimmybui1995@gmail.com>
2024-10-08 16:27:20 +0700

    changed head logo

--- components/customHeader.tsx
+++ components/customHeader.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import Text from '@/components/customText';
 import { Ionicons } from '@expo/vector-icons';
 import { primaryColor } from '@/constant/color';
 
+const appLogo = require('@/assets/images/phone-logo.png')
 interface CustomHeaderProps {
   isGuest: boolean;
   firstname: string;
@@ -11,9 +12,9 @@ interface CustomHeaderProps {
 
 const CustomHeader: React.FC<CustomHeaderProps> = ({ isGuest, firstname }) => {
   return (
-    <View className="flex-row justify-between items-center p-4 pt-16 bg-white pb-4">
-      <View className="flex-row items-center pt-2 ">
-        <Ionicons name="logo-octocat" size={30} color={primaryColor} />
+    <View className="flex-row justify-between items-center p-4 pt-16 bg-white pb-0">
+      <View className="flex-row items-center">
+        <Image source={appLogo} className="w-8 h-12" />
         <Text className="ml-2 text-lg font-NunitoSemiBold">
           {isGuest ? 'Hi!' : `Hi, ${firstname}!`}
         </Text>
